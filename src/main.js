34aec1f97128bc9162ac55567ee630fb43f9fcd8
kurtis chan <kurtis.chan.1@live.com>
2023-02-08 18:57:22 +0900

    antialiasing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import '../styles/main.css'
-
 import * as THREE from 'three';
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
@@ -10,7 +8,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#c')
+  canvas: document.querySelector('#c'),
+  antialias: true
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
