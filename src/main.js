055f00d0bb9af0fdc5b40dea2003e0869a6fc514
kurtis chan <kurtis.chan.1@live.com>
2023-02-14 21:44:24 +0900

    animation loop now functional
    i think it can do with some optimizations from here on tbh

--- src/main.js
+++ src/main.js
@@ -19,7 +19,6 @@ init();
 animate();
 
 function init() {
-
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 4.8; // 2.8
     camera.position.x = -0.8; // -1.5
@@ -174,18 +173,19 @@ function onResize(event) {
 
 function animate() {
 
+    requestAnimationFrame(animate);
+
     target.x = (1 - mouse.x) * 0.0002;
     target.y = (1 - mouse.y) * 0.0002;
 
-    // instaGalaxyMesh.rotateY(0.02);
-    // fbGalaxyMesh.rotateY(-0.01);
-    // linkedinGalaxyMesh.rotateY(0.006);
-    // gitGalaxyMesh.rotateY(-0.008);
+    instaGalaxyMesh.rotateY(0.02);
+    fbGalaxyMesh.rotateY(-0.01);
+    linkedinGalaxyMesh.rotateY(0.006);
+    gitGalaxyMesh.rotateY(-0.008);
 
     camera.rotation.x += 0.05 * (target.y - camera.rotation.x);
     camera.rotation.y += 0.05 * (target.x - camera.rotation.y);
 
-    requestAnimationFrame(animate);
     renderer.render(scene, camera);
 
 }
