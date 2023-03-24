27f96d3c502b75226b96b489d63d0dfe1be84b0e
kurtis <kurtis.chan.1@live.com>
2023-03-25 00:47:36 +0900

    updated camera positioning

--- src/main.js
+++ src/main.js
@@ -25,8 +25,9 @@ animate();
 
 function init() {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 4.5; // 2.8
-    camera.position.x = -0.8; // -1.5
+    camera.position.z = 4.8; // 2.8
+    camera.position.x = -0.2; // -1.5
+    camera.position.y = 0.2;
 
     scene = new THREE.Scene();
 
