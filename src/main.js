df975cd80113c6b939962ad35fc8598be99feae7
kurtis chan <kurtis.chan.1@live.com>
2023-03-28 14:09:52 +0900

    import gsap
    next step is to animate content based on visible section

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '../styles/main.css'
 import * as THREE from 'three';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'gsap';
 
 let camera, scene, raycaster, renderer, objLoader;
 
