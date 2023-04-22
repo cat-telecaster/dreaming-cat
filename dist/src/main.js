28f16175c16ec4aa02a97bb2393108464c3c8127
kurtis <kurtis.chan.1@live.com>
2023-04-23 05:37:08 +0900

    Update main.js

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,4 +1,4 @@
-import './dist/styles/main.css'
+import '../styles/main.css'
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { gsap } from 'gsap';
