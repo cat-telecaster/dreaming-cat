57424eb682b4b69ce86f0c4c6055efe114a1fdd9
kurtis <kurtis.chan.1@live.com>
2023-04-23 05:50:50 +0900

    Updates

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import '../styles/main.css'
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+//import * as THREE from 'three';
+//import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import SplitType from 'split-type';
