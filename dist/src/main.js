ef617fcc2198c450a2ddb06f790c4935981b8f99
kurtis <kurtis.chan.1@live.com>
2023-04-23 05:35:44 +0900

    deploy

--- dist/src/main.js
+++ dist/src/main.js
@@ -1,4 +1,4 @@
-import '../styles/main.css'
+import './dist/styles/main.css'
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { gsap } from 'gsap';
