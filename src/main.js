abb852686096db4d95e0da695a3bafd5aa85cfa6
kurtis <kurtis.chan.1@live.com>
2023-04-23 05:16:25 +0900

    Updates

---
+++ src/main.js
@@ -0,0 +1,428 @@
+import '../styles/main.css'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import SplitType from 'split-type';
+
+let camera, scene, raycaster, renderer, objLoader;
+
+let clickableObjects = [];
+
+const backgroundImg = new Image();
+backgroundImg.src = './assets/background/render5_1080.png';
+
+// Load external GLTF models from directory
+let instaGalaxyMesh;    // mesh for insta galaxy
+let fbGalaxyMesh;       // mesh for fb galaxy
+let linkedinGalaxyMesh; // mesh for linkedin galaxy
+let gitGalaxyMesh;      // mesh for git galaxy
+let taroMesh;           // mesh for azathoth
+
+const mouse = new THREE.Vector2();
+const target = new THREE.Vector2();
+const windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+
+let mouseWheelY = 0;
+let translatedMouseWheelY = 75; // Initial position for camera.position.y
+
+let scrollY = window.scrollY;
+
+init();
+
+// GSAP Animations
+gsap.registerPlugin(ScrollTrigger);
+
+var thisSection = '';
+const sections = gsap.utils.toArray('.section');
+
+const timeline1 = gsap.timeline({paused: true})
+    .from('#about-me', {
+        opacity: 0,
+        delay: 1.5,
+        duration: 0.5,
+        ease: 'power2.in',
+    })
+    .from('.me-info', {
+        opacity: 0,
+        stagger: 1,
+        delay: 0.3,
+        duration: 0.5,
+        ease: 'power2.in',
+    });
+
+
+const timeline2 = gsap.timeline({paused: true})
+    .from('#work-hist', {
+        opacity: 0,
+        delay: 1.2,
+        duration: 0.5,
+        ease: 'power2.in',
+    })
+    .from('.wrk-entry', {
+        opacity: 0,
+        stagger: 1,
+        delay: 0.3,
+        duration: 0.5,
+        ease: 'power2.in',
+    });
+
+const linkPageText = new SplitType('.link-page-header')
+const timeline3 = gsap.timeline({paused: true})
+    .from('.word', {
+        opacity: 0,
+        stagger: 0.1,
+        delay: 0.7,
+        duration: 0.8,
+        ease: 'power2.in',
+    })
+    .to('body', {
+        backgroundColor: "transparent",
+    })
+    .to('#c', {
+        opacity: 1,
+        duration: 0.5,
+    });
+
+sections.forEach( (item) => {
+    let currentFunction = item.getAttribute('id');
+    ScrollTrigger.create({
+        trigger: item,
+        start: "top top",
+        scrub: 1,
+        onEnter: ({ progress, direction, isActive }) => {
+            // console.log('enter down ' + thisSection);
+            // console.log(currentFunction)
+            getToAnimation(currentFunction);
+        },
+        onEnterBack: ({ progress, direction, isActive }) => {
+            // console.log('enter up ' + thisSection);
+            // console.log(currentFunction)
+            getFromAnimation(currentFunction);
+        },
+      });
+});
+ScrollTrigger.create({
+    snap: 1 / 3,
+});
+
+function getToAnimation(fnstring) {
+    switch (fnstring) {
+        case '1': toFirst(); break;
+        case '2': toSecond(); break;
+        case '3': toThird(); break;
+    }
+};
+
+function getFromAnimation(fnstring) {
+    switch (fnstring) {
+        case '1': fromFirst(); break;
+        case '2': fromSecond(); break;
+        case '3': fromThird(); break;
+    }
+};
+
+function toFirst() {
+    thisSection = '1';
+    gsap.to('body', {
+        duration: 0.5,
+        backgroundColor: '#9575cd'
+    })
+    timeline1.restart();
+};
+
+function fromFirst() {
+    thisSection = '1';
+    timeline1.restart();
+    timeline2.reverse();
+};
+
+function toSecond() {
+    thisSection = '2';
+    timeline2.restart();
+    timeline1.reverse();
+};
+
+function fromSecond() {
+    thisSection = '2';
+    timeline2.restart();
+    timeline1.restart();
+    timeline3.reverse();
+};
+
+function toThird() {
+    thisSection = '3';
+    timeline3.restart();
+    timeline2.reverse();
+};
+
+function fromThird() {
+    thisSection = '3';
+    // timeline3.reverse();
+    // timeline2.restart();
+};
+
+const greetingText = new SplitType('#welcome-page')
+gsap.to('.name-intro', {
+    opacity: 1,
+    stagger: 1,
+    delay: 2,
+    duration: 1,
+    ease: 'power2.in',
+});
+
+animate();
+
+function init() {
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 4.8; // 2.8
+    camera.position.x = 0.3; // -1.5
+
+    scene = new THREE.Scene();
+
+    objLoader = new GLTFLoader();
+
+    // Azathoth
+    objLoader.load( './assets/models/sleeping_taro2.gltf', ( gltf ) => {
+        taroMesh = gltf.scene;
+        taroMesh.scale.set(10, 10, 10);
+        scene.add(taroMesh);
+        taroMesh.position.x = 0;
+        taroMesh.position.y = 0;
+        taroMesh.position.z = 0;
+        taroMesh.rotation.y = 3;
+    }, undefined, function ( error ) {
+        console.error( error );
+    });
+
+    // Insta galaxy
+    objLoader.load( './assets/models/insta_galaxy_obj2.gltf', ( gltf ) => {
+        instaGalaxyMesh = gltf.scene;
+        instaGalaxyMesh.scale.set(0.5, 0.5, 0.5);
+        scene.add(instaGalaxyMesh);
+        instaGalaxyMesh.position.x = -0.4;
+        instaGalaxyMesh.position.y = 1.5;
+        instaGalaxyMesh.position.z = 0;
+        instaGalaxyMesh.rotation.y = -0.4;
+        instaGalaxyMesh.rotation.z = 0.4;
+        instaGalaxyMesh.userData = {
+            URL: 'https://www.instagram.com/kayc.jpg'
+        };
+        clickableObjects.push(instaGalaxyMesh);
+    }, undefined, function ( error ) {
+        console.error( error );
+    });
+
+    // Facebook galaxy
+    objLoader.load( './assets/models/fb_galaxy_obj1.gltf', ( gltf ) => {
+        fbGalaxyMesh = gltf.scene;
+        fbGalaxyMesh.scale.set(-0.7, -0.7, 0.7);
+        scene.add(fbGalaxyMesh);
+        fbGalaxyMesh.position.x = 0.8;
+        fbGalaxyMesh.position.y = 0.9;
+        fbGalaxyMesh.position.z = 0.2;
+        fbGalaxyMesh.rotation.y = -0.8;
+        fbGalaxyMesh.rotation.x = 0.7;
+        fbGalaxyMesh.userData = {
+            URL: 'https://www.facebook.com/crunchtofu'
+        };
+        clickableObjects.push(fbGalaxyMesh);
+    }, undefined, function ( error ) {
+        console.error( error );
+    });
+
+    // LinkedIn galaxy
+    objLoader.load( './assets/models/linkedin_galaxy_obj1.gltf', ( gltf ) => {
+        linkedinGalaxyMesh = gltf.scene;
+        linkedinGalaxyMesh.scale.set(1.1, 1.1, 1.1);
+        scene.add(linkedinGalaxyMesh);
+        linkedinGalaxyMesh.position.x = -0.3;
+        linkedinGalaxyMesh.position.y = 0.4;
+        linkedinGalaxyMesh.position.z = 0.1;
+        linkedinGalaxyMesh.rotation.z = 0.3;
+        linkedinGalaxyMesh.rotation.x = 0.3;
+        linkedinGalaxyMesh.userData = {
+            URL: 'https://www.linkedin.com/in/kayconnect'
+        };
+        clickableObjects.push(linkedinGalaxyMesh);
+    }, undefined, function ( error ) {
+        console.error( error );
+    });
+
+    // GitHub galaxy
+    objLoader.load( './assets/models/git_galaxy_obj1.gltf', ( gltf ) => {
+        gitGalaxyMesh = gltf.scene;
+        gitGalaxyMesh.scale.set(-1, -1, 1);
+        scene.add(gitGalaxyMesh);
+        gitGalaxyMesh.position.x = -0.7;
+        gitGalaxyMesh.position.y = 1.0;
+        gitGalaxyMesh.position.z = -1.1;
+        gitGalaxyMesh.rotation.z = -0.3;
+        gitGalaxyMesh.rotation.x = -0.2;
+        gitGalaxyMesh.userData = {
+            URL: 'https://github.com/cat-telecaster'
+        };
+        clickableObjects.push(gitGalaxyMesh);
+    }, undefined, function ( error ) {
+        console.error( error );
+    });
+
+    // Stars
+    function addStar () {
+        const geometry = new THREE.SphereGeometry(0.05, 24, 24);
+        const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+        const star = new THREE.Mesh( geometry, material );
+
+        const [x, y, z] = Array(3).fill().map( () => THREE.MathUtils.randFloatSpread( 200 ) );
+
+        star.position.set(x, y, z);
+        scene.add(star);
+    };
+
+    Array(5000).fill().forEach(addStar);
+
+    // Lighting
+
+    const pointLight = new THREE.PointLight(0xCFCFCF);
+    pointLight.position.set(-50,22,25);
+
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+
+    scene.add(pointLight, ambientLight);
+
+    backgroundImg.onload = function () {
+        scene.background = new THREE.TextureLoader().load(backgroundImg.src);
+    };
+
+    // Helpers
+    // const lightHelper = new THREE.PointLightHelper(pointLight);
+    // const gridHelper = new THREE.GridHelper(200,50)
+    // scene.add(lightHelper, gridHelper);
+
+    raycaster = new THREE.Raycaster();
+
+    renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#c'),
+        antialias: true
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    document.addEventListener('mousemove', onMouseMove, false);
+    document.addEventListener('wheel', onMouseWheel, false);
+    document.addEventListener('click', onMouseClick, false);
+    window.addEventListener('resize', onResize, false);
+    window.addEventListener('scroll', onScroll, false);
+}
+
+function onMouseMove(event) {
+
+    mouse.x = (event.clientX - windowHalf.x);
+    mouse.y = (event.clientY - windowHalf.y);
+
+}
+
+function onMouseWheel(event) {
+
+    mouseWheelY = event.deltaY * 0.0007;
+
+}
+
+function onMouseClick(event) {
+    mouse.x = ((event.clientX / window.innerWidth) * 2 - 1);
+    mouse.y = (-(event.clientY / window.innerHeight) * 2 + 1);
+    raycaster.setFromCamera( mouse, camera );
+	const intersects = raycaster.intersectObjects(clickableObjects);
+    if (intersects.length > 0) {
+        const { URL } = intersects[0].object.parent.userData;
+        window.open(URL, '_blank');
+    }
+}
+
+function onResize(event) {
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    windowHalf.set(width / 2, height / 2);
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+
+    setBackground(scene, backgroundImg.width, backgroundImg.height);
+
+}
+
+function onScroll(event) {
+
+    scrollY = window.scrollY;
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    raycaster.setFromCamera( mouse, camera );
+	const intersects = raycaster.intersectObjects(clickableObjects);
+    if (intersects.length > 0) {
+        document.documentElement.style.cursor = "pointer";
+    } else {
+        document.documentElement.style.cursor = "initial";
+    }
+
+    translatedMouseWheelY = - scrollY / window.innerHeight + 5 * 0.6096;
+    translatedMouseWheelY *= 10
+
+    camera.position.y = translatedMouseWheelY;
+
+    target.x = (1 - mouse.x) * 0.0002;
+    target.y = (1 - mouse.y) * 0.0002;
+
+    if (instaGalaxyMesh) instaGalaxyMesh.rotateY(0.02);
+    if (fbGalaxyMesh) fbGalaxyMesh.rotateY(-0.01);
+    if (linkedinGalaxyMesh) linkedinGalaxyMesh.rotateY(0.006);
+    if (gitGalaxyMesh) gitGalaxyMesh.rotateY(-0.008);
+
+    camera.rotation.x += 0.05 * (target.y - camera.rotation.x);
+    camera.rotation.y += 0.05 * (target.x - camera.rotation.y);
+
+    renderer.render(scene, camera);
+
+}
+
+function setBackground(scene, backgroundImageWidth, backgroundImageHeight) {
+    const windowSize = function(withScrollBar) {
+        let wid = 0;
+        let hei = 0;
+        if (typeof window.innerWidth != "undefined") {
+            wid = window.innerWidth;
+            hei = window.innerHeight;
+        }
+        else {
+            if (document.documentElement.clientWidth == 0) {
+                wid = document.body.clientWidth;
+                hei = document.body.clientHeight;
+            }
+            else {
+                wid = document.documentElement.clientWidth;
+                hei = document.documentElement.clientHeight;
+            }
+        }
+        return { width: wid - (withScrollBar ? (wid - document.body.offsetWidth + 1) : 0), height: hei };
+    };
+
+    if (scene.background) {
+
+        const size = windowSize(true);
+        const factor = (backgroundImageWidth / backgroundImageHeight) / (size.width / size.height);
+
+        scene.background.offset.x = factor > 1 ? (1 - 1 / factor) / 2 : 0;
+        scene.background.offset.y = factor > 1 ? 0 : (1 - factor) / 2;
+
+        scene.background.repeat.x = factor > 1 ? 1 / factor : 1;
+        scene.background.repeat.y = factor > 1 ? 1 : factor;
+    }
+}
---
+++ src/old.js.txt
@@ -0,0 +1,200 @@
+import '../styles/main.css'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#c'),
+  antialias: true
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.z = 4.8; // 2.8
+camera.position.x = -0.8; // -1.5
+
+// Load external GLTF models from directory
+let instaGalaxyMesh;    // mesh for insta galaxy
+let fbGalaxyMesh;       // mesh for fb galaxy
+let linkedinGalaxyMesh; // mesh for linkedin galaxy
+let gitGalaxyMesh;      // mesh for git galaxy
+let taroMesh;           // mesh for azathoth
+const objLoader = new GLTFLoader();
+
+// Azathoth
+objLoader.load( './assets/models/sleeping_taro2.gltf', ( gltf ) => {
+  taroMesh = gltf.scene;
+  taroMesh.scale.set(10, 10, 10);
+  scene.add(taroMesh);
+  taroMesh.position.x = 0;
+  taroMesh.position.y = 0;
+  taroMesh.position.z = 0;
+  taroMesh.rotation.y = 3;
+}, undefined, function ( error ) {
+  console.error( error );
+});
+
+// Insta galaxy
+objLoader.load( './assets/models/insta_galaxy_obj2.gltf', ( gltf ) => {
+  instaGalaxyMesh = gltf.scene;
+  instaGalaxyMesh.scale.set(0.5, 0.5, 0.5);
+  scene.add(instaGalaxyMesh);
+  instaGalaxyMesh.position.x = -0.4;
+  instaGalaxyMesh.position.y = 1.5;
+  instaGalaxyMesh.position.z = 0;
+  instaGalaxyMesh.rotation.y = -0.4;
+  instaGalaxyMesh.rotation.z = 0.4;
+}, undefined, function ( error ) {
+  console.error( error );
+});
+
+// Facebook galaxy
+objLoader.load( './assets/models/fb_galaxy_obj1.gltf', ( gltf ) => {
+  fbGalaxyMesh = gltf.scene;
+  fbGalaxyMesh.scale.set(-0.7, -0.7, 0.7);
+  scene.add(fbGalaxyMesh);
+  fbGalaxyMesh.position.x = 0.8;
+  fbGalaxyMesh.position.y = 0.9;
+  fbGalaxyMesh.position.z = 0.2;
+  fbGalaxyMesh.rotation.y = -0.8;
+  fbGalaxyMesh.rotation.x = 0.7;
+}, undefined, function ( error ) {
+  console.error( error );
+});
+
+// LinkedIn galaxy
+objLoader.load( './assets/models/linkedin_galaxy_obj1.gltf', ( gltf ) => {
+  linkedinGalaxyMesh = gltf.scene;
+  linkedinGalaxyMesh.scale.set(1.1, 1.1, 1.1);
+  scene.add(linkedinGalaxyMesh);
+  linkedinGalaxyMesh.position.x = -0.3;
+  linkedinGalaxyMesh.position.y = 0.4;
+  linkedinGalaxyMesh.position.z = 0.1;
+  linkedinGalaxyMesh.rotation.z = 0.3;
+  linkedinGalaxyMesh.rotation.x = 0.3;
+}, undefined, function ( error ) {
+  console.error( error );
+});
+
+// GitHub galaxy
+objLoader.load( './assets/models/git_galaxy_obj1.gltf', ( gltf ) => {
+  gitGalaxyMesh = gltf.scene;
+  gitGalaxyMesh.scale.set(-1, -1, 1);
+  scene.add(gitGalaxyMesh);
+  gitGalaxyMesh.position.x = -0.7;
+  gitGalaxyMesh.position.y = 1.0;
+  gitGalaxyMesh.position.z = -1.1;
+  gitGalaxyMesh.rotation.z = -0.3;
+  gitGalaxyMesh.rotation.x = -0.2;
+}, undefined, function ( error ) {
+  console.error( error );
+});
+
+// Stars
+function addStar () {
+  const geometry = new THREE.SphereGeometry(0.05, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map( () => THREE.MathUtils.randFloatSpread( 100 ) );
+
+  star.position.set(x, y, z);
+  scene.add(star);
+};
+
+Array(500).fill().forEach(addStar);
+
+// Lighting
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(10,15,5);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+
+scene.add(pointLight, ambientLight);
+
+// Background
+
+function setBackground(scene, backgroundImageWidth, backgroundImageHeight) {
+  const windowSize = function(withScrollBar) {
+    let wid = 0;
+    let hei = 0;
+    if (typeof window.innerWidth != "undefined") {
+      wid = window.innerWidth;
+      hei = window.innerHeight;
+    }
+    else {
+      if (document.documentElement.clientWidth == 0) {
+        wid = document.body.clientWidth;
+        hei = document.body.clientHeight;
+      }
+      else {
+        wid = document.documentElement.clientWidth;
+        hei = document.documentElement.clientHeight;
+      }
+    }
+    return { width: wid - (withScrollBar ? (wid - document.body.offsetWidth + 1) : 0), height: hei };
+  };
+
+  if (scene.background) {
+
+    const size = windowSize(true);
+    const factor = (backgroundImageWidth / backgroundImageHeight) / (size.width / size.height);
+
+    scene.background.offset.x = factor > 1 ? (1 - 1 / factor) / 2 : 0;
+    scene.background.offset.y = factor > 1 ? 0 : (1 - factor) / 2;
+
+    scene.background.repeat.x = factor > 1 ? 1 / factor : 1;
+    scene.background.repeat.y = factor > 1 ? 1 : factor;
+  }
+}
+
+const backgroundImg = new Image();
+backgroundImg.onload = function () {
+  scene.background = new THREE.TextureLoader().load(backgroundImg.src);
+  setBackground(scene, backgroundImg.width, backgroundImg.height);
+};
+backgroundImg.src = './assets/background/render5_1080.png';
+
+// Helpers
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200,50)
+scene.add(lightHelper, gridHelper);
+
+// Camera controls
+
+const controls = new OrbitControls(camera, renderer.domElement);
+/*const moveCamera = () => {
+  const t = document.body.getBoundingClientRect().top;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.01;
+};
+*/
+
+// Animation Loop
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  const canvas = renderer.domElement;
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
+
+  instaGalaxyMesh.rotateY(0.02);
+  fbGalaxyMesh.rotateY(-0.01);
+  linkedinGalaxyMesh.rotateY(0.006);
+  gitGalaxyMesh.rotateY(-0.008);
+
+  //document.body.onscroll = moveCamera();
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
